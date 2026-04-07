f0757cce806c50d819c2945e64fbf6f514f632b7
Deng Kuat <denq1738@gmail.com>
2026-04-07 20:16:46 +0200

    update: made the navbar responsive to different screen sizes

--- E-Commerce/src/Routes/Navbar.tsx
+++ E-Commerce/src/Routes/Navbar.tsx
@@ -2,9 +2,20 @@ import { Link } from "react-router-dom"
 
 export const Navbar = () => {
   return (
-    <nav className='flex justify-between p-8 shadow-2xl px-15 font-bold text-center'>
-      <Link to='/' className="text-4xl">E-Commerce</Link>
-      <Link to='/login' className="text-2xl border  rounded-2xl py-1 px-4 font-semibold ">Login</Link>
-    </nav>
+    <nav className="flex flex-col sm:flex-row justify-between items-center p-4 sm:p-6 lg:p-8 shadow-2xl font-bold text-center">
+  <Link 
+    to="/" 
+    className="text-3xl sm:text-4xl lg:text-5xl mb-2 sm:mb-0"
+  >
+    E-Commerce
+  </Link>
+
+  <Link 
+    to="/login" 
+    className="text-lg sm:text-2xl lg:text-2xl border rounded-2xl py-1 px-4 font-semibold"
+  >
+    Sign Out
+  </Link>
+</nav>
   )
 }
