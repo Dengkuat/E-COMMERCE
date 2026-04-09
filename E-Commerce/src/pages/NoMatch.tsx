186750cda42452b2988b3e7fe5f2372276d30c50
Deng Kuat <denq1738@gmail.com>
2026-04-09 09:13:56 +0200

    NEW: Create a no match component {catch all} to handle senarials where there are no available routes

---
+++ E-Commerce/src/pages/NoMatch.tsx
@@ -0,0 +1,8 @@
+
+export const NoMatch = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-6xl font-bold">No Page found</p>
+    </div>
+  )
+}
