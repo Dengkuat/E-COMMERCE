b2cf36c7dd461186145335ceb4d9a5d21ea84e23
Deng Kuat <denq1738@gmail.com>
2026-04-09 10:21:13 +0200

    Update:Updates the paths to home, categories and products in the AppRoutes

--- E-Commerce/src/Routes/AppRoutes.tsx
+++ E-Commerce/src/Routes/AppRoutes.tsx
@@ -1,15 +1,17 @@
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { Login } from '../pages/Login'
 import { NoMatch } from '../pages/NoMatch'
+import { Categories } from '../pages/Categories'
 
 export const AppRoutes = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='*' element={<NoMatch/>}/>
+        <Route path='/categories' element={<Home />}/>
+        <Route path='/categories/products/:id' element={<Categories />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<NoMatch />} />
       </Routes>
     </div>
   )
