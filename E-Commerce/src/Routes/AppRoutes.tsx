50e7056817fb9da4f0316832843f3c1c49703186
Deng Kuat <denq1738@gmail.com>
2026-04-09 22:57:34 +0200

    NEW ROUTE: Create a dynamic routes for the products

--- E-Commerce/src/Routes/AppRoutes.tsx
+++ E-Commerce/src/Routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { Home } from '../pages/Home'
 import { Login } from '../pages/Login'
 import { NoMatch } from '../pages/NoMatch'
 import { Categories } from '../pages/Categories'
+import { Products } from '../pages/Products'
 
 export const AppRoutes = () => {
   return (
@@ -10,6 +11,7 @@ export const AppRoutes = () => {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/categories/:id' element={<Categories />} />
+        <Route path='/categories/:id/products/:ProductId' element={<Products/>}/>
         <Route path='/login' element={<Login />} />
         <Route path='*' element={<NoMatch />} />
       </Routes>
